716dd5ff52f0ce34cb23f864b3f2245a1df864f7
richie0226 <970071@stu.kjes.tp.edu.tw>
2021-03-13 17:43:00 +0800

    initial commit

---
+++ src/main/java/com/richie/A.java
@@ -0,0 +1,13 @@
+package com.richie;
+
+public class A {
+    String id;
+    int price;
+    int discount;
+    int total;
+    public Customer(String id, int price,int discount,int total) {
+        this.id = id;
+        this.price = price;
+        this.discount = discount;
+        this.total = total;
+}
