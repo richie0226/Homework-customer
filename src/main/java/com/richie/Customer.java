79be78cf6aaec94863259927241e927c19c209f1
richie0226 <970071@stu.kjes.tp.edu.tw>
2021-03-13 17:06:54 +0800

    initial commit

---
+++ src/main/java/com/richie/Customer.java
@@ -0,0 +1,18 @@
+package com.richie;
+
+public class Customer {
+}
+public static void main(String[] args) {
+    int[]id =
+    int[]price
+    int[]discount
+    int[]total
+Customer[]customers={
+            new Customer(id:"0013",price:490,discount:0,total:490),
+            new Customer(id:"0052",price:1000,discount:100,total:900),
+            new Customer(id:"0081",price:290,discount:0,total:290),
+            new Customer(id:"2122",price:2000,discount:200,total:1800),
+}
+
+    System.out.println();
+    }
