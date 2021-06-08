10505558aa307ca672cf1190bd7507e2f26162cc
richie0226 <970071@stu.kjes.tp.edu.tw>
2021-06-08 14:24:46 +0800

    Car

---
+++ src/main/java/com/richie/CarDemo.java
@@ -0,0 +1,14 @@
+package com.richie;
+
+import javax.swing.*;
+
+public class CarDemo {
+private JPanel mainPainel;
+private JTextField textField;
+private JButton button1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setContentPane((new CarDemo()));
+    }
+}
